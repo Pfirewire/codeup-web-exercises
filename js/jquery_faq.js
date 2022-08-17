e372da2bb065be39932e7cab164d5355b67cf698
Stephen Pflug <stephen@pflug.us>
2022-08-17 16:57:35 -0500

    traversing exercises complete

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,6 +1,8 @@
 "use strict";
 
 $(function () {
+
+    // ------ Attributes and CSS ------
     $("dd").addClass("invisible");
     $("#toggle-visibility").click(function () {
         $("dd").toggleClass("invisible");
@@ -8,4 +10,42 @@ $(function () {
     $("dt").click(function () {
         $(this).toggleClass("highlighted");
     });
+
+    // ------ Traversing ------
+    $("#add-yellow-btn").click(function () {
+        $(".park-list").each(function (index) {
+            $(this).children().last().css("background-color", "yellow");
+            $(this).children().last().css("color", "black");
+        });
+    });
+    $("h3").click(function () {
+        $(this).next().children().first().css("font-weight", "bold");
+    });
+    $("li").click(function () {
+        $(this).parent().children().first().css("color", "blue");
+    });
+
+    $(".btn").click(function () {
+        let tempImg = $("#center-img").attr("src");
+        if ($(this).attr("id") === "left-btn") {
+            swapLeftTwoImages();
+        } else if ($(this).attr("id") === "right-btn") {
+            swapRightTwoImages();
+        } else {
+            Math.random() < .5 ? swapLeftTwoImages() : swapRightTwoImages();
+        }
+    });
+
+    function swapLeftTwoImages() {
+        let tempImg = $("#center-img").attr("src");
+        $("#center-img").attr("src", $("#left-img").attr("src"));
+        $("#left-img").attr("src", tempImg);
+    }
+
+    function swapRightTwoImages() {
+        let tempImg = $("#center-img").attr("src");
+        $("#center-img").attr("src", $("#right-img").attr("src"));
+        $("#right-img").attr("src", tempImg);
+    }
+
 });
