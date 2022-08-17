830bb2df6a09f5e0dbd3c97749fb78a6400f532a
Stephen Pflug <stephen@pflug.us>
2022-08-17 13:06:51 -0500

    selectors class selectors exercises complete

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,13 +1,20 @@
 "use strict";
 
 $(function() {
-    alert(`The DOM has finished loading!`);
 
+    // ------ jQuery Introduction Exercises ------
+
+    // alert(`The DOM has finished loading!`);
+
+
+    // ------ ID Selectors Exercises ------
     // alert($("#hello-world").html());
-    alert($("#option-one").html());
+    // alert($("#option-one").html());
 
-    //when two elements have the same id, only the first is selected
+    // when two elements have the same id, only the first is selected
 
-
+    $(".codeup").css("border", "1px solid red");
+    // confirmed when removing "codeup" class from element it takes the border away
+    // confirmed adding "codeup" class to element creates a border
 
 });
