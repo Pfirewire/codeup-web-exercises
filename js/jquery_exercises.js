b9084311f47485c703463a6bab3f5ba568a153df
Stephen Pflug <stephen@pflug.us>
2022-08-17 14:00:36 -0500

    mouse events exercises complete

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -15,12 +15,29 @@ $(function() {
     // confirmed adding "codeup" class to element creates a border
 
     // ------ Element Selectors Exercises ------
-    $("li").css("font-size", "20px");
+    // $("li").css("font-size", "20px");
     // $("h1").css("background-color", "brown");
     // $("p").css("background-color", "brown");
     // $("li").css("background-color", "brown");
-    alert($("h1").html());
+    // alert($("h1").html());
 
     // ------ Multiple Selectors Exercises ------
-    $("h1, p, li").css("background-color", "brown");
+    // $("h1, p, li").css("background-color", "yellow");
+    // $("h1, p, li").css("color", "black");
+
+
+    // ------ Mouse Events Exercises ------
+    $("h1").click(function () {
+        // alert("h1 was clicked");
+        $(this).css("background-color", "yellow");
+        $(this).css("color", "black");
+    });
+    $("p").dblclick(function () {
+        $(this).css("font-size", "18px");
+    });
+    $("li").hover(function () {
+        $("li").css("color", "red");
+    } , function () {
+        $("li").css("color", "white");
+    });
 });
