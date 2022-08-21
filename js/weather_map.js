e8c51fc402b5f204f36ef708a174299756a36cb3
Stephen Pflug <stephen@pflug.us>
2022-08-21 16:08:46 -0500

    completed functions to return high and low temp for a full day in 5 day forecast

--- js/weather_map.js
+++ js/weather_map.js
@@ -10,6 +10,62 @@ $(() => {
     // takes in string and returns same string with the first letters of each word capitalized
     const firstLettersCapitalized = string => string.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
 
+    //gets weather data from WeatherMap and displays in navbar
+    const displayCurrentWeatherData = () => {
+        $.get("http://api.openweathermap.org/data/2.5/weather", {
+            APPID: OPEN_WEATHER_APPID,
+            q:     "San Antonio, US",
+            units: "imperial"
+        }).done(data => {
+            console.log(data);
+            $("#current-icon").html(`<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">`);
+            $("#current-weather-description").html(firstLettersCapitalized(data.weather[0].description));
+            $("#current-temp").html(`${parseInt(data.main.temp)} F`);
+            $("#current-feels-like").html(`Feels Like ${parseInt(data.main.feels_like)} F`);
+            $("#current-humidity").html(`${data.main.humidity}% Humidity`)
+        });
+    }
+
+    //find high temp from weathermap, inputting data list of 3 hour interval weather data and amount of days from today
+    const highTemp = (data, day) => {
+        let highTemp = 0;
+        for(let i=0; i<8; i++) {
+            if(highTemp < data.list[i + (day*8) - 8].main.temp_max) {
+                highTemp = data.list[i + (day*8) - 8].main.temp_max;
+            }
+        }
+        return parseInt(highTemp);
+    }
+
+    //find low temp from weathermap, inputting data list of 3 hour interval weather data and amount of days from today
+    const lowTemp = (data, day) => {
+        let lowTemp = 1000;
+        for(let i=0; i<8; i++) {
+            if(lowTemp > data.list[i + (day*8) - 8].main.temp_min) {
+                lowTemp = data.list[i + (day*8) - 8].main.temp_min;
+            }
+        }
+        return parseInt(lowTemp);
+    }
+
+    //gets forecast from WeatherMap and displays 5 day forecast on page
+    const displayForecast = () => {
+        $.get("http://api.openweathermap.org/data/2.5/forecast", {
+            APPID: OPEN_WEATHER_APPID,
+            q:     "San Antonio, US",
+            units: "imperial"
+        }).done(data => {
+            console.log(data);
+                $("#upcoming-tomorrow-card").html(`
+                    <ul>
+                        <li>High: ${highTemp(data, 1)}</li>
+                        <li>Low: ${lowTemp(data, 1)}</li>
+                        <li></li>
+                    </ul>
+                `);
+        });
+    }
+
 
     // global variables
     mapboxgl.accessToken = MAPBOX_KEY;
@@ -27,27 +83,11 @@ $(() => {
         map.setFog({}); // Set the default atmosphere style
     });
 
-    //
-    const displayCurrentWeatherData = () => {
-        let weatherData;
-        $.get("http://api.openweathermap.org/data/2.5/weather", {
-            APPID: OPEN_WEATHER_APPID,
-            q:     "San Antonio, US",
-            units: "imperial"
-        }).done(data => {
-            console.log(data);
-            $("#current-icon").html(`<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">`);
-            $("#current-weather-description").html(firstLettersCapitalized(data.weather[0].description));
-            $("#current-temp").html(`${parseInt(data.main.temp)} F`);
-            $("#current-feels-like").html(`Feels Like ${parseInt(data.main.feels_like)} F`);
-        });
-    }
 
+
+
+    //display weather data
     displayCurrentWeatherData();
-
-
-
+    displayForecast();
 
 });
-
-//
